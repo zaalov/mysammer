977a32e695c5b97155325ce27ce8bc977b8b3175
zaalovich <zaalovich@icloud.com>
2017-09-05 22:26:22 +0300

    Домашнее задание №1

---
+++ src/Mysammer.java
@@ -0,0 +1,9 @@
+public class Mysammer {
+    public static void main(String[] args) {
+            System.out.println("Как я проведу лето 2017?");
+            System.out.println("Буду гулять на свежем воздухе");
+            System.out.println("Катать на вейке, тусить с друзьями");
+            System.out.println("Открою для себя новое место в Мире");
+            System.out.println("А после начну изучать Java");
+    }
+}
